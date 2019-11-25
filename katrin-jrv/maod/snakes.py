9d16b014e16eee28681d9b3017c189b9402c3054
Katrin Jrv <kjarv45@gmail.com>
2019-11-25 11:07:18 +0200

    first python

---
+++ katrin-jrv/maod/snakes.py
@@ -0,0 +1,11 @@
+print ("Sss..")
+name = input ("Whatss your name, bypassser?")
+print ("Iss your name really " + name + "?")
+lengthOfName = len(name)
+if lengthOfName > 7:
+    print ("You have such a long name!")
+elif lengthOfName > 4:
+    print ("You have a pretty average name.")
+else:
+    print ("Aw shit, here we go again..")
+    
