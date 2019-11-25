918650cf39605877f773aff08143fd2f748e70f3
Edera-Elizee <ederaelizee@gmail.com>
2019-11-25 11:17:06 +0200

    Kõik läks hästi

---
+++ kassid/koerad/koerad/Dog.cs
@@ -0,0 +1,41 @@
+using System;
+
+
+namespace dogs {
+	  class dogs {
+	  	static void Main(string[] args) {
+		       Console.WriteLine($"Auh!");
+		       Console.WriteLine("Mis on sinu nimi?");
+		       string name;
+		       name= Console.ReadLine();
+		       Console.WriteLine("Kas su nimi on tõesti " + name + "?");
+		       if (name.Length > 10) {
+		       	  Console.WriteLine("Küll sul on pikk nimi!");
+		       } else {
+			  Console.WriteLine("Sul on lühike nimi."
+			  }
+			  if (name.Length > 10) }
+			  Console.WriteLine("Küll sul on pikk nimi!");
+			  } else if (name.Length > 5) {
+			  Console.WriteLine("Sul on keskmine pikkusega nimi.");
+			  } else {
+			  Console.WriteLine("Sul on lühike nimi.");
+		       }
+		       switch (name.Length) {
+		       case 5: {
+		       Console.WriteLine("Nii palju korruseid on meie majal.");
+		       break;
+		       }
+		       case 2: {
+		       Console.WriteLine("Nii palju eri numbreid tunneb arvuti.");
+		       break;
+		       }
+		       default: {
+		       Console.WriteLine("Öösiti on külmem kui väljas.");
+		       break;
+		       }
+		       {
+		    
+}
+}
+}
